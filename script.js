9660b3fb1ef3bcdb7756fd2bd585dfb9ee22c1f7
adeyusuf211 <adeyusuf005@gmail.com>
2021-11-05 20:19:20 +0700

    Menghapus penurunan kasus aktir hari ini

--- script.js
+++ script.js
@@ -92,8 +92,8 @@ function cards(data) {
                     <h2>${ (parseFloat(data.active) / parseFloat(data.cases) * parseFloat(100)).toFixed(2) } %</h2>
                 </div>
                 <div class="case">
-                    <h4>Pengurangan Kasus Aktif (Hari Ini).</h4>
-                    <h2>${ (parseFloat(data.todayRecovered) + parseFloat(data.todayDeaths)).toLocaleString('id-ID', '.') }</h2>
+                    <h4>Kritis.</h4>
+                    <h2>${ parseFloat(data.critical).toLocaleString('id-ID', '.') }</h2>
                 </div>
             </div>
             <div class="cases">
@@ -174,8 +174,8 @@ function allCards(data) {
                     <h2>${ (parseFloat(data.active) / parseFloat(data.cases) * parseFloat(100)).toFixed(2) } %</h2>
                 </div>
                 <div class="case">
-                    <h4>Pengurangan Kasus Aktif (Hari Ini).</h4>
-                    <h2>${ (parseFloat(data.todayRecovered) + parseFloat(data.todayDeaths)).toLocaleString('id-ID', '.') }</h2>
+                    <h4>Kritis</h4>
+                    <h2>${ parseFloat(data.critical).toLocaleString('id-ID', '.') }</h2>
                 </div>
             </div>
             <div class="cases">
